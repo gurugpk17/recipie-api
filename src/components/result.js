75099647f33ed24ebdda2d03a0bdd23bca304118
gurugpk17 <gurumoorthy.2002@srit.org>
2024-06-28 23:26:53 +0530

    updated the code

--- src/components/result.js
+++ src/components/result.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import Img from '../images/cook.png'
-export default function Result({value}) {
+import React from 'react';
+import Img from '../images/cook.png';
+
+export default function Result({ value }) {
     return (
         <div className="result">
-            <h1>Hello {value} Welcome!</h1>
-            <img src={Img} width="250px"/>
-            <p>Lets cook together {value} :)</p>
+            <h1>Hello {value}, Welcome!</h1>
+            <img src={Img} width="250px" alt="Cooking illustration" />
+            <p>Let's cook together, {value} :)</p>
         </div>
     );
 }
